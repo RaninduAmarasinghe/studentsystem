47640aecbce6466a352c57b55804311e208333aa
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2024-07-27 20:34:04 +0530

    create getters and setter

--- src/main/java/com/ranindu/studentsys/model/Student.java
+++ src/main/java/com/ranindu/studentsys/model/Student.java
@@ -1,4 +1,42 @@
 package com.ranindu.studentsys.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Student {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String Name;
+    private String address;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
